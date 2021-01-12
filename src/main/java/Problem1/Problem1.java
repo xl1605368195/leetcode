7f8184b11fbf4df57a80d224e833295afca0754b
user01 <1870011370@qq.com>
2021-01-12 13:41:16 +0800

    twoSum

---
+++ src/main/java/Problem1/Problem1.java
@@ -0,0 +1,35 @@
+package Problem1;
+
+import java.util.HashMap;
+
+public class Problem1 {
+
+    // https://leetcode-cn.com/problems/two-sum/
+
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                result[0] = i;
+                result[1] = map.get(target - nums[i]);
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return result;
+    }
+
+    public int[] twoSum2(int[] nums, int target) {
+        int[] result = new int[2];
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    result[0] = i;
+                    result[1] = j;
+                }
+            }
+        }
+        return result;
+    }
+}
